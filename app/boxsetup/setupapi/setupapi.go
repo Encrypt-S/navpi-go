3a6e9b14125e4365cab41939c60f801f5a2ca38c
red010b37 <paul@creativetech.io>
2018-02-24 12:09:01 +1300

    Changes range to handle apis and detect if using localhost

--- app/boxsetup/setupapi/setupapi.go
+++ app/boxsetup/setupapi/setupapi.go
@@ -2,51 +2,75 @@ package setupapi
 
 import (
 	"fmt"
-	"github.com/NAVCoin/navpi-go/app/conf"
 	"github.com/NAVCoin/navpi-go/app/middleware"
 	"github.com/gorilla/mux"
-	"log"
 	"net"
 	"net/http"
+	"github.com/NAVCoin/navpi-go/app/api"
+	"encoding/json"
 )
+
+
 
 func InitSetupHandlers(r *mux.Router, prefix string) {
 
 	var nameSpace string = "setup"
 
-	var path_ip_detect string = fmt.Sprintf("/%s/%s/v1/detectip", prefix, nameSpace)
+	var path_ip_detect string = fmt.Sprintf("/%s/%s/v1/setrange", prefix, nameSpace)
 
-	r.Handle(path_ip_detect, middleware.Adapt(detectIpV1Handler(), middleware.Notify()))
+	r.Handle(path_ip_detect, middleware.Adapt(rangeSetHandler(), middleware.Notify()))
 
 }
 
-func detectIpV1Handler() http.Handler {
+func rangeSetHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		host, port, err := net.SplitHostPort(r.RemoteAddr)
+		host, _, err := net.SplitHostPort(r.RemoteAddr)
 
-		log.Println("host :: ", host)
+		w.WriteHeader(http.StatusBadRequest)
 
-		log.Println("port :: ", port)
-
+		// If there is no host found we need to
 		if err != nil || host == "" {
-			log.Println("err :: ", err)
+			apiResp := api.APIResponse{}
+			apiResp.Error.Message = "Cannot detect the host"
+
+			jsonValue, _ := json.Marshal(apiResp)
+
+			w.WriteHeader(http.StatusInternalServerError)s
+			w.Write(jsonValue)
+
+			return
 		}
 
-		if host == "::1" {
+		if host != "::1" {
 
-			log.Println("localhost")
-
-		} else {
-
-			log.Println("not localhost")
 
 		}
 
-		conf.AppConf.DetectedIp = host
-		conf.SaveAppConfig()
-
-		fmt.Fprintf(w, "Hi there, I ran the middleware, I love %s!", r.URL.Path[1:])
+		//host, port, err := net.SplitHostPort(r.RemoteAddr)
+		//
+		//log.Println("host :: ", host)
+		//
+		//log.Println("port :: ", port)
+		//
+		//if err != nil || host == "" {
+		//	log.Println("err :: ", err)
+		//}
+		//
+		//if host == "::1" {
+		//
+		//	log.Println("localhost")
+		//
+		//} else {
+		//
+		//	log.Println("not localhost")
+		//
+		//}
+		//
+		//conf.AppConf.DetectedIp = host
+		//conf.SaveAppConfig()
+		//
+		//fmt.Fprintf(w, "Hi there, I ran the middleware, I love %s!", r.URL.Path[1:])
 
 	})
 }
