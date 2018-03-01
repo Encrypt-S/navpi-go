57e8780824c211c477959b50e0ed4d5f9fbb5f2a
zanuka <git@zanuka.com>
2018-03-02 11:46:23 +1300

    cleanup test comments

--- app/boxsetup/setupapi/setupapi_test.go
+++ app/boxsetup/setupapi/setupapi_test.go
@@ -1,18 +1,3 @@
 package setupapi
 
-// basic test setup :: gofight API Handler
-
-// protectUIHandler...
-
-//func TestInitSetupHandlers(t *testing.T) {
-//	r := gofight.New()
-//
-//	r.GET("/").
-//	// turn on the debug mode.
-//		SetDebug(true).
-//		Run(InitSetupHandlers(r., "api"), func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
-//
-//		assert.Equal(t, "Hello World", r.Body.String())
-//		assert.Equal(t, http.StatusOK, r.Code)
-//	})
-//}
+// in progress...
