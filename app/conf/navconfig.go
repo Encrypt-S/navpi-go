53c4d565d7d2f64eadb073401372cb6b4a8a1c44
red010b37 <paul@creativetech.io>
2018-03-05 21:22:04 +1300

    Adds random rpc user and password generator to the system

--- app/conf/navconfig.go
+++ app/conf/navconfig.go
@@ -1,23 +1,47 @@
 package conf
-//
-//import (
-//	"errors"
-//	"io/ioutil"
-//	"os"
-//	"regexp"
-//)
-//
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
 type NavConfig struct {
 	RpcUser     string `json:"rpcUser"`
 	RpcPassword string `json:"rpcPassword"`
 }
 
 
+// CreateRPCDetails generate rpc details for this run
 func CreateRPCDetails () {
-	//TODO: create an random user and password on start
-	NavConf.RpcPassword = "hi"
-	NavConf.RpcUser = "userYo"
 
+	NavConf.RpcUser, _ = generateRandomString(32)
+	NavConf.RpcPassword, _ = generateRandomString(32)
+
+}
+
+// GenerateRandomBytes returns securely generated random bytes.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// GenerateRandomString returns a URL-safe, base64 encoded
+// securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func generateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
 }
 
 
