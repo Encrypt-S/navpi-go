8f1ebfd3f2c6f06dea98e8064d8bfa6a4927b7d0
red010b37 <paul@creativetech.io>
2018-03-23 16:23:58 +1300

    Adds generate jwt util

---
+++ app/utils/generatejwt.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+// TODO: Generate this and store it in the app config
+var signingKey = []byte("secret")
+
+// GenerateJWT accepts a duration of
+// time that will be added to the current
+// time to generate the expiry date
+func GenerateJWT(exp time.Duration) string  {
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	/* Create a map to store our claims */
+	claims := token.Claims.(jwt.MapClaims)
+
+	/* Set token claims */
+	//claims["admin"] = true
+	claims["exp"] = time.Now().Add(exp).Unix()
+
+	/* Sign the token with our secret */
+	tokenString, _ := token.SignedString(signingKey)
+
+	return tokenString
+
+}
